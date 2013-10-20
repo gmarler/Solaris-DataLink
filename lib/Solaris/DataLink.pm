97ae33ddce3f4798e4e982aaffe44eb08cf87070
Gordon Marler <gmarler@gmarler.com>
2013-10-19 23:59:00 -0400

    Updated with attributes, and enum named subtypes for DataLink

--- lib/Solaris/DataLink.pm
+++ lib/Solaris/DataLink.pm
@@ -1,10 +1,21 @@
 package Solaris::DataLink;
 
 use Moose;
+use Moose::Util::TypeConstraints;
 
-has [ 'name' ] => ( 
-  is  => 'ro',
-  isa => 'Str',
-);
+has [ 'name' ]  => ( is  => 'ro', isa => 'Str', );
+has [ 'zone' ]  => ( is  => 'ro', isa => 'Str', );
+enum 'Solaris::DataLink::Class', [ qw/aggr bridge etherstub
+                                      iptun part phys vlan vnic/ ];
+has [ 'class' ] => ( is  => 'ro',
+                     isa => 'Solaris::DataLink::Class', );
+has [ 'mtu' ]  => ( is  => 'rw', isa => 'Int', default => 0, );
+
+enum 'Solaris::DataLink::State', [ qw/up down unknown/ ];
+
+has [ 'state' ]  => ( is  => 'rw',
+                      isa => 'Solaris::DataLink::State', );
+
+
 
 1;
