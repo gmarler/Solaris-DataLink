6427a9384e2aaff9b1ae34c6ac8a55fafda2ebc1
Gordon Marler <gmarler@gmarler.com>
2013-10-20 00:16:32 -0400

    Test Solaris::DataLink::Class attribute enforcement

--- t/lib/TestsFor/Solaris/DataLink.pm
+++ t/lib/TestsFor/Solaris/DataLink.pm
@@ -11,5 +11,28 @@ sub test_constructor {
     'The object the constructor returns';
 }
 
+sub test_datalink_class {
+  my ($test, $report) = @_;
+  my @valid_class = qw( aggr bridge etherstub
+                        iptun part phys vlan vnic);
+  my @invalid_class = qw( barf junk crazy tunnel aggregation
+                          virtual_nic ether ethernet VLAN );
+  my $dl_obj;
+
+  foreach my $class (@valid_class) {
+    $dl_obj = Solaris::DataLink->new(name => 'e1000g0',
+                                     class => $class);
+    isa_ok $dl_obj, $test->class_name,
+      "DataLink with class $class";
+  }
+
+  foreach my $class (@invalid_class) {
+    throws_ok { $dl_obj = Solaris::DataLink->new(name => 'e1000g0',
+                                                 class => $class) }
+      '/Attribute.+?does\snot\spass\sthe\stype\sconstraint/',
+      "bad class '$class'";
+  }
+}
+
 1;
 
